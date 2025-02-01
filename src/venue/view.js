4623ef802ecce72657cacdb2316b19b4085827c1
Dennis Claassen <declaassen@gmail.com>
2025-02-01 09:33:44 +0100

    Fix js lint issues

--- src/venue/view.js
+++ src/venue/view.js
@@ -21,5 +21,5 @@
  */
 
 /* eslint-disable no-console */
-console.log("Hello World! (from dc23-tea-venue block)");
+console.log( 'Hello World! (from dc23-tea-venue block)' );
 /* eslint-enable no-console */
