501c628c971b0f59bf671b503ab62b87085d482a
Dennis Claassen <declaassen@gmail.com>
2025-02-02 14:17:51 +0100

    Update post-panel-row.js

--- src/lib/components/post-panel-row.js
+++ src/lib/components/post-panel-row.js
@@ -6,13 +6,13 @@ import { __experimentalHStack as HStack } from '@wordpress/components';
 /**
  * Post panel row for usage in WordPress sidebar.
  *
- * Taken from @wordpress/block-editor, where its not ecposed. Thid does rely
+ * Taken from @wordpress/block-editor, where its not exposed. This does rely
  * on the css classes from that exact implementation.
  *
  * @param {Object}  props
  * @param {string}  props.className
  * @param {string}  props.label
- * @param {Element} props.childrem
+ * @param {Element} props.children
  *
  * @return {Element} Row with label and content.
  */
