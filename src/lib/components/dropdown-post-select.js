1139a99be648f52f44a4f1880fea101bf11f2661
Dennis Claassen <declaassen@gmail.com>
2025-02-04 07:08:48 +0100

    Create organizer sidebar option

---
+++ src/lib/components/dropdown-post-select.js
@@ -0,0 +1,10 @@
+/**
+ * Dropdown element to select a wp_post from any post_type.
+ *
+ * @return {Element} The dropdown.
+ */
+export function DropdownPostSelect() {
+    return (
+        <div/>
+    );
+}
