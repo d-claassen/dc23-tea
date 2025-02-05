0acd905c3c3edc88c64a34d20a84b4b540dddaff
dennis <dennis@yoast.com>
2025-02-05 22:04:53 +0100

    Block for organizer url

---
+++ src/organizer-url/edit.js
@@ -0,0 +1,69 @@
+/**
+ * WordPress dependencies.
+ */
+import { useBlockProps } from '@wordpress/block-editor';
+const { useSelect } = require( '@wordpress/data' );
+
+/**
+ * Internal dependencies.
+ */
+import './editor.scss';
+
+/**
+ * The edit function describes the structure of your block in the context of the
+ * editor. This represents what the editor will render when the block is used.
+ *
+ * @param {Object} props
+ * @param {Object} props.context
+ * @param {string} props.context.postType
+ * @param {number} props.context.postId
+ * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
+ *
+ * @return {Element} Element to render.
+ */
+function Content( { context: { postType, postId } } ) {
+	const { url } = useSelect(
+		( select ) => {
+			const { getEntityRecord, getEditedEntityRecord } = select( 'core' );
+
+			const originalEvent = getEntityRecord( 'postType', 'tribe_events', postId );  // Trigger resolver.
+			const event = getEditedEntityRecord( 'postType', 'tribe_events', postId );
+			const { _EventOrganizerID } = event?.meta;
+
+			const organizer = getEntityRecord( 'postType', 'tribe_organizer', _EventOrganizerID );
+			const { _OrganizerWebsite } = organizer?.meta || {};
+
+			return {
+				url: _OrganizerWebsite,
+			};
+		},
+		[ postId ]
+	);
+
+	return (
+		<div { ...useBlockProps() }>
+			<a href={ url }>{ url }</a>
+		</div>
+	);
+}
+function Placeholder() {
+	return (
+		<div { ...useBlockProps() }>
+			<a href="https://example.org">Organizer URL</a>
+		</div>
+	);
+}
+
+export default function Edit( { context } ) {
+	const { postType, postId } = context;
+
+	return (
+		<>
+			{ postId && postType ? (
+				<Content context={ context } />
+			) : (
+				<Placeholder />
+			) }
+		</>
+	);
+}
---
+++ src/organizer-url/render.php
@@ -0,0 +1,9 @@
+<?php
+$eventOrganizerId = (int) get_post_meta( $block->context['postId'], '_EventOrganizerID', true );
+$organizerUrl = get_post_meta( $eventOrganizerId, '_OrganizerWebsite', true );
+?>
+<div <?php echo get_block_wrapper_attributes(); ?>>
+	<a href="<?php echo esc_url( $organizerUrl ) ?>" class="wp-block-dc23-tea-organizer__url">
+		<?php echo esc_html( $organizerUrl ) ?>
+	</a>
+</div>
