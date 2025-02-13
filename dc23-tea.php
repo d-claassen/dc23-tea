f9aef96d46f77d25dc29ebd0cdd97c2929c30013
d-claassen <d-claassen@users.noreply.github.com>
2025-02-13 21:00:43 +0000

    0.1.3-alpha.0

--- dc23-tea.php
+++ dc23-tea.php
@@ -5,7 +5,7 @@
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Requires Plugins:  wordpress-seo, the-events-calendar
- * Version:           0.1.0
+ * Version:           0.1.3-alpha.0
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
