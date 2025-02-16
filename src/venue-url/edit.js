21e2b44c8697563aa77ee7d27acf0df8745f2e03
Dennis Claassen <declaassen@gmail.com>
2025-02-16 01:59:05 +0100

    Update edit.js

--- src/venue-url/edit.js
+++ src/venue-url/edit.js
@@ -1,23 +1,11 @@
 /**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
-import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
+ * WordPress dependencies.
  */
 import { useBlockProps } from '@wordpress/block-editor';
+const { useSelect } = require( '@wordpress/data' );
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
+ * Internal dependencies.
  */
 import './editor.scss';
 
@@ -25,14 +13,67 @@ import './editor.scss';
  * The edit function describes the structure of your block in the context of the
  * editor. This represents what the editor will render when the block is used.
  *
+ * @param {Object} props
+ * @param {Object} props.context
+ * @param {number} props.context.postId
  * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+function Content( { context: { postId } } ) {
+	const { url } = useSelect(
+		( select ) => {
+			const { getEntityRecord, getEditedEntityRecord } = select( 'core' );
+
+			const event = getEditedEntityRecord(
+				'postType',
+				'tribe_events',
+				postId
+			);
+			const { _EventVenueID } = event?.meta;
+
+			const venue = getEntityRecord(
+				'postType',
+				'tribe_venue',
+				_EventVenueID
+			);
+			const { _VenueURL } = venue?.meta || {};
+
+			return {
+				url: _VenueURL,
+			};
+		},
+		[ postId ]
+	);
+
 	return (
-		<p { ...useBlockProps() }>
-			{ __( 'Venue URL – hello from the editor!', 'venue-url' ) }
-		</p>
+		<div { ...useBlockProps() }>
+			<a href={ url }>{ url }</a>
+		</div>
+	);
+}
+function Placeholder() {
+	return (
+		<div { ...useBlockProps() }>
+			<a href="https://example.org">Venue URL</a>
+		</div>
+	);
+}
+
+export default function Edit( { context } ) {
+	const { postType, postId } = context;
+
+	if ( postType !== 'tribe_events' ) {
+		return null;
+	}
+
+	return (
+		<>
+			{ postId && postType ? (
+				<Content context={ context } />
+			) : (
+				<Placeholder />
+			) }
+		</>
 	);
 }
