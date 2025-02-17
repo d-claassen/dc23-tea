b8169add021c5d324ef6f470de93cbcc06ec1fea
Dennis Claassen <declaassen@gmail.com>
2025-02-18 00:13:41 +0100

    Update render.php

--- src/venue-address/render.php
+++ src/venue-address/render.php
@@ -4,18 +4,17 @@ $venueAddress = get_post_meta( $eventVenueId, '_VenueAddress', true );
 $venueCity = get_post_meta( $eventVenueId, '_VenueCity', true );
 $venueCountry = get_post_meta( $eventVenueId, '_VenueCountry', true );
 ?>
-<address <?php echo get_block_wrapper_attributes(); ?>>
-    <?php if ( ! empty( $venueAddress ) ): ?>
-	<?php echo esc_html( $venueAddress ) ?><br/>
-    <?php endif; ?>
-    
-    <?php if ( ! empty( $venueCity ) ): ?>
-	<?php echo esc_html( $venueCity ) ?>,
-    <?php endif; ?>
-    
-    <?php if ( ! empty( $venueCountry ) ): ?>
-	<?php echo esc_html( $venueCountry ) ?><br/>
-    <?php endif; ?>
 
-    
+<address <?php echo get_block_wrapper_attributes(); ?>>
+	<?php if ( ! empty( $venueAddress ) ): ?>
+	<?php echo esc_html( $venueAddress ) ?><br/>
+	<?php endif; ?>
+
+	<?php if ( ! empty( $venueCity ) ): ?>
+	<?php echo esc_html( $venueCity ) ?>,
+	<?php endif; ?>
+
+	<?php if ( ! empty( $venueCountry ) ): ?>
+	<?php echo esc_html( $venueCountry ) ?><br/>
+	<?php endif; ?>
 </address>
