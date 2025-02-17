6a8ebd4cf4149d0c4ce29cd9642c300009279a34
Dennis Claassen <declaassen@gmail.com>
2025-02-17 20:43:06 +0100

    Update render.php

--- src/venue-address/render.php
+++ src/venue-address/render.php
@@ -1,8 +1,21 @@
 <?php
-/**
- * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
- */
+$eventVenueId = (int) get_post_meta( $block->context['postId'], '_EventVenueID', true );
+$venueAddress = get_post_meta( $eventVenueId, '_VenueAddress', true );
+$venueCity = get_post_meta( $eventVenueId, '_VenueCity', true );
+$venueCountry = get_post_meta( $eventVenueId, '_VenueCountry', true );
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
-	<?php esc_html_e( 'Venue address – hello from a dynamic block!', 'venue-address' ); ?>
-</p>
+<address <?php echo get_block_wrapper_attributes(); ?>>
+    <?php if ( ! empty( $venueAddress ) ): ?>
+	<?php echo esc_html( $venueAddress ) ?><br/>
+    <?php endif; ?>
+    
+    <?php if ( ! empty( $venueCity ) ): ?>
+	<?php echo esc_html( $venueCity ) ?>,
+    <?php endif; ?>
+    
+    <?php if ( ! empty( $venueCountry ) ): ?>
+	<?php echo esc_html( $venueCountry ) ?><br/>
+    <?php endif; ?>
+
+    
+</address>
