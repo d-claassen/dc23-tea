e16fa8c1ef641614a47bc9de561ea93760d12e02
Dennis Claassen <declaassen@gmail.com>
2025-02-26 00:23:47 +0100

    Update render.php

--- src/venue-name/render.php
+++ src/venue-name/render.php
@@ -1,7 +1,7 @@
 <?php
-$event_venue_i  = (int) get_post_meta( $block->context['postId'], '_EventVenueID', true );
-$event_venue   = get_post( $event_venue_id );
-$venue_name    = ( $event_venue instanceof WP_Post ) ? $eventVenue->post_title : '';
+$event_venue_id = (int) get_post_meta( $block->context['postId'], '_EventVenueID', true );
+$event_venue    = get_post( $event_venue_id );
+$venue_name     = ( $event_venue instanceof WP_Post ) ? $event_venue->post_title : '';
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<?php echo esc_html( $venue_name ); ?>
