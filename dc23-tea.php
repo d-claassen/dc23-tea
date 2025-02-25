33e30beb81bfcde16f58b194897648e4dbe5c26a
dennis <dennis@yoast.com>
2025-02-25 21:21:55 +0100

    Enhance Event schema with person roles

--- dc23-tea.php
+++ dc23-tea.php
@@ -17,6 +17,8 @@
  * @package Dc23
  */
 
+use Yoast\WP\SEO\Context\Meta_Tags_Context;
+
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
@@ -39,6 +41,49 @@ function dc23_tea_block_init() {
 }
 add_action( 'init', 'dc23_tea_block_init' );
 
+function dc23_tea_schema_init() {
+	\add_filter( 'wpseo_schema_event', 'enhance_event_with_role', 11, 2 );
+}
+add_action( 'init', 'dc23_tea_schema_init' );
+
+
+function enhance_event_with_role( $event_data, $context ) {
+	\assert( $context instanceof Meta_Tags_Context );
+
+	if ( ! ( \is_single() && \get_post_type() === 'tribe_events' ) ) {
+		return $event_data;
+	}
+
+	$event_id = \get_the_ID();
+
+	$roles = get_post_meta( $event_id, '_EventRole', true );
+	if ( empty( $roles ) ) {
+		return $event_data;
+	}
+
+	if ( $context->site_represents !== 'person' ) {
+		return $event_data;
+	}
+
+	$person_reference = [
+		'@id'  => \YoastSEO()->helpers->schema->id->get_user_schema_id( $context->site_user_id, $context ),
+	];
+
+	if ( in_array( 'Attending', $roles, true ) && empty( $event_data[ 'attendee' ] ) ) {
+		$event_data['attendee'] = $person_reference;
+	}
+	if ( in_array( 'Organizing', $roles, true ) && empty( $event_data[ 'organizer' ] ) ) {
+		$event_data['organizer'] = $person_reference;
+	}
+	if ( in_array( 'Sponsoring', $roles, true ) && empty( $event_data[ 'sponsor' ] ) ) {
+		$event_data['sponsor'] = $person_reference;
+	}
+	if ( in_array( 'Performing', $roles, true ) && empty( $event_data[ 'performer' ] ) ) {
+		$event_data['performer'] = $person_reference;
+	}
+
+	return $event_data;
+}
 
 
 /**
