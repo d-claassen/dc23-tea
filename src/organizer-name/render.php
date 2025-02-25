43f82d8a4248828c72712bf657319607dd47ea6f
Dennis Claassen <declaassen@gmail.com>
2025-02-26 00:43:11 +0100

    Update render.php

--- src/organizer-name/render.php
+++ src/organizer-name/render.php
@@ -1,8 +1,8 @@
 <?php
-$eventOrganizerId = (int) get_post_meta( $block->context['postId'], '_EventOrganizerID', true );
-$eventOrganizer   = get_post( $eventOrganizerId );
-$organizerTitle   = ( $eventOrganizer instanceof WP_Post ) ? $eventOrganizer->post_title : '';
+$event_organizer_id = (int) get_post_meta( $block->context['postId'], '_EventOrganizerID', true );
+$event_organizer    = get_post( $event_organizer_id );
+$organizer_title    = ( $event_organizer instanceof WP_Post ) ? $event_organizer->post_title : '';
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
-	<?php echo esc_html( $organizerTitle ); ?>
+	<?php echo esc_html( $organizer_title ); ?>
 </div>
