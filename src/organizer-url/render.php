d099e5f866ed7837483e3d15c5ace65ec13a7aec
Dennis Claassen <declaassen@gmail.com>
2025-02-25 23:00:49 +0100

    Update organizer-url/render.php

--- src/organizer-url/render.php
+++ src/organizer-url/render.php
@@ -1,6 +1,6 @@
 <?php
-$event_organizer_d = (int) get_post_meta( $block->context['postId'], '_EventOrganizerID', true );
-$organizer_url      = get_post_meta( $event_organizer_id, '_OrganizerWebsite', true );
+$event_organizer_id = (int) get_post_meta( $block->context['postId'], '_EventOrganizerID', true );
+$organizer_url       = get_post_meta( $event_organizer_id, '_OrganizerWebsite', true );
 ?>
 <div <?php echo get_block_wrapper_attributes(); ?>>
 	<a href="<?php echo esc_url( $organizer_url ); ?>" class="wp-block-dc23-tea-organizer__url">
