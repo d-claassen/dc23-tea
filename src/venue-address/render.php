e00e28cdd835e624596512dad858c741d84ec15c
Dennis Claassen <declaassen@gmail.com>
2025-02-26 01:08:53 +0100

    Update render.php

--- src/venue-address/render.php
+++ src/venue-address/render.php
@@ -1,10 +1,10 @@
 <?php
 $event_venue_id = (int) get_post_meta( $block->context['postId'], '_EventVenueID', true );
-$venue_address = get_post_meta( $event_venue_id, '_VenueAddress', true );
-$venue_city    = get_post_meta( $event_venue_id, '_VenueCity', true );
-$venue_country = get_post_meta( $event_venue_id, '_VenueCountry', true );
-$venue_region  = get_post_meta( $event_venue_id, '_VenueStateProvince', true );
-$venue_zip     = get_post_meta( $event_venue_id, '_VenueZip', true );
+$venue_address  = get_post_meta( $event_venue_id, '_VenueAddress', true );
+$venue_city     = get_post_meta( $event_venue_id, '_VenueCity', true );
+$venue_country  = get_post_meta( $event_venue_id, '_VenueCountry', true );
+$venue_region   = get_post_meta( $event_venue_id, '_VenueStateProvince', true );
+$venue_zip      = get_post_meta( $event_venue_id, '_VenueZip', true );
 ?>
 
 <address <?php echo get_block_wrapper_attributes(); ?>>
