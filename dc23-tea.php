11d7a1d9eaf1fd1d6fe56c05d7c0c5b506036db4
Dennis Claassen <declaassen@gmail.com>
2025-03-05 07:40:26 +0100

    Composer autoload

--- dc23-tea.php
+++ dc23-tea.php
@@ -25,6 +25,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+require_once 'vendor/autoload.php';
+
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
