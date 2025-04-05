ea41d356d0a41df399f2b4f912fa1dcdc8be1648
Dennis Claassen <declaassen@gmail.com>
2025-04-05 02:47:49 +0200

    Update at-a-glance.spec.js

--- specs/at-a-glance.spec.js
+++ specs/at-a-glance.spec.js
@@ -3,10 +3,12 @@
  */
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
-test('Verify "At a Glance" widget presence', async ({ page, admin }) => {
-  // Navigate to the WordPress admin dashboard
-  await admin.visitAdminPage('index.php');
-
-  // Assert that the "At a Glance" widget is visible
-  await expect(page.locator('#dashboard_right_now')).toBeVisible();
+test.describe('"At a glance" widget integration', () => {
+  test('Verify widget presence', async ({ page, admin }) => {
+    // Navigate to the WordPress admin dashboard
+    await admin.visitAdminPage('index.php');
+  
+    // Assert that the "At a Glance" widget is visible
+    await expect(page.locator('#dashboard_right_now')).toBeVisible();
+  });
 });
