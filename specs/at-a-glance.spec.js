544894684891e5100d204aa48e21e10b65134bfb
Dennis Claassen <declaassen@gmail.com>
2025-04-05 02:56:01 +0200

    Update at-a-glance.spec.js

--- specs/at-a-glance.spec.js
+++ specs/at-a-glance.spec.js
@@ -4,11 +4,19 @@
 const { test, expect } = require( '@wordpress/e2e-test-utils-playwright' );
 
 test.describe('"At a glance" widget integration', () => {
-  test('Verify widget presence', async ({ page, admin }) => {
+  test('widget presence', async ({ page, admin }) => {
     // Navigate to the WordPress admin dashboard
     await admin.visitAdminPage('index.php');
   
     // Assert that the "At a Glance" widget is visible
     await expect(page.locator('#dashboard_right_now')).toBeVisible();
+  });
+  
+  test('correct event count', async ({ page, admin }) => {
+    // Navigate to the WordPress admin dashboard
+    await admin.visitAdminPage('index.php');
+
+    // Check the count
+    await expect(page.locator('.tribe-events-count')).toHaveText('0 Events');
   });
 });
