11c8ec8b56dc99a3c47135c3958c55f19186cb63
Dennis Claassen <declaassen@gmail.com>
2025-04-07 07:14:11 +0200

    Update activity.spec.js

--- specs/activity.spec.js
+++ specs/activity.spec.js
@@ -14,4 +14,20 @@ test.describe('Dashboard Activity widget', async () => {
         // Should not contain any events (yet)
         await expect(activityWidget).not.toContainText('Test Event Title');
     });
+    
+    test('Published event appears in the Activity dashboard widget', async ({ page, admin }) => {
+        // Add a new event (classic editor assumed)
+        await admin.openAdminPage('post-new.php', 'post_type=tribe_events');
+        await page.fill('#title', 'Test Event Title');
+        await page.fill('#content', 'This is the event content.');
+        await page.click('#publish');
+
+        // Go back to the dashboard
+        await admin.visitAdminPage('index.php');
+        // Find the widget
+        const activityWidget = page.locator('#dashboard_activity');
+
+        // Check that the event title shows up
+        await expect(activityWidget).toContainText('Test Event Title');
+    });
 });
