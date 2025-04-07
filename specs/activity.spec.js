2952b9bc4b04454d4713160f54446b0429648325
Dennis Claassen <declaassen@gmail.com>
2025-04-08 00:24:37 +0200

    Update activity.spec.js

--- specs/activity.spec.js
+++ specs/activity.spec.js
@@ -15,7 +15,7 @@ test.describe('Dashboard Activity widget', async () => {
         await expect(activityWidget).not.toContainText('Test Event Title');
     });
     
-    test('Published event appears in the Activity dashboard widget', async ({ page, admin }) => {
+    test.skip('Published event appears in the Activity dashboard widget', async ({ page, admin }) => {
         // Add a new event (classic editor assumed)
         await admin.visitAdminPage('post-new.php', 'post_type=tribe_events');
         await page.fill('#title', 'Test Event Title');
@@ -30,4 +30,29 @@ test.describe('Dashboard Activity widget', async () => {
         // Check that the event title shows up
         await expect(activityWidget).toContainText('Test Event Title');
     });
+    
+
+test('Published event appears in the Activity dashboard widget (block editor)', async ({ page, admin, editor }) => {
+
+
+        await admin.visitAdminPage('post-new.php', 'post_type=tribe_events');
+
+  // Fill event title
+  await page.locator('textarea.editor-post-title__input').fill('Test Event Title');
+
+  // Type into the editor using Editor.canvas
+  await editor.canvas.type('This is the event content.');
+
+  // Publish the post
+    await admin.publishPost();
+  //await page.click('button:has-text("Publish")');
+  //await page.click('button:has-text("Publish")');
+  await page.waitForSelector('div:has-text("published")');
+
+  // Go to Dashboard
+        await admin.visitAdminPage('index.php');
+  const activityWidget = page.locator('#dashboard_activity');
+
+  await expect(activityWidget).toContainText('Test Event Title');
+});
 });
