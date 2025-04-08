f33ece843cdb822809a48b19b502a5beb5b43749
dennis <dennis@yoast.com>
2025-04-08 21:30:24 +0200

    Fix glance events

--- specs/at-a-glance.spec.js
+++ specs/at-a-glance.spec.js
@@ -7,11 +7,11 @@ test.describe('"At a glance" widget integration', () => {
   test('widget presence', async ({ page, admin }) => {
     // Navigate to the WordPress admin dashboard
     await admin.visitAdminPage('index.php');
-  
+
     // Assert that the "At a Glance" widget is visible
     await expect(page.locator('#dashboard_right_now')).toBeVisible();
   });
-  
+
   test('correct event count', async ({ page, admin }) => {
     // Navigate to the WordPress admin dashboard
     await admin.visitAdminPage('index.php');
@@ -19,21 +19,17 @@ test.describe('"At a glance" widget integration', () => {
     // Check the count
     await expect(page.locator('.tribe_events-count')).toHaveText('0 Events');
   });
-  
-  test.fixme('correct 1 event count', async ({ page, admin }) => {
-    // Go to Add Event
-    await admin.visitAdminPage('post-new.php', 'post_type=tribe_events');
 
-    // Fill out the event title and content
-    await page.fill('#title', 'My Test Event');
-    await page.fill('#content', 'This is a sample event description.');
+  test('correct 1 event count', async ({ page, admin, editor }) => {
+    // Create a new event
+		await admin.createNewPost( {
+			title: 'My Test Event',
+			postType: 'tribe_events',
+			status: 'publish',
+		} );
 
     // Publish
-    await page.click('#publish');
-    await page.waitForURL(/edit\.php.*post_type=tribe_events/);
-  
-    // Confirm it's published
-    await expect(page.locator('.notice-success')).toContainText('published');
+    await editor.publishPost();
 
     // Navigate to the WordPress admin dashboard
     await admin.visitAdminPage('index.php');
