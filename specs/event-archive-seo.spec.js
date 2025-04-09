c7cd7c02f7cb792dba00d7d99e9a1e9373aca79b
Dennis Claassen <declaassen@gmail.com>
2025-04-09 20:11:36 +0200

    Update event-archive-seo.spec.js

--- specs/event-archive-seo.spec.js
+++ specs/event-archive-seo.spec.js
@@ -4,8 +4,12 @@ test.describe('Yoast SEO on Events archive', () => {
 	test.beforeEach(async ({ page, admin }) => {
 		// Set a custom archive title for events in Yoast SEO
 		await admin.visitAdminPage('admin.php', 'page=wpseo_page_settings#/post-type/events');
-		await page.locator('#title-tribe_events-ptarchive').fill('Test Event Archive Title %%sep%% %%sitename%%');
-		await page.locator('#submit').click();
+		
+		const input = await page.getByLabel('SEO title');
+		await input.fill('Test Event Archive Title %%sep%% %%sitename%%');
+		
+		// await page.locator('#title-tribe_events-ptarchive').fill('Test Event Archive Title %%sep%% %%sitename%%');
+		// await page.locator('#submit').click();
 	});
 
 	test('SEO title does not reflect Yoast SEO setting', async ({ page }) => {
