cdb0e3ce664a11b8821fbe1da1642a790929283b
Dennis Claassen <declaassen@gmail.com>
2025-04-10 08:06:56 +0200

    Update event-archive-seo.spec.js

--- specs/event-archive-seo.spec.js
+++ specs/event-archive-seo.spec.js
@@ -5,26 +5,23 @@ test.describe('Yoast SEO on Events archive', () => {
 		// Set a custom archive title for events in Yoast SEO
 		await admin.visitAdminPage('admin.php', 'page=wpseo_page_settings#/post-type/events');
 		
+		// close ai modal
 		const aiModal = await page.getByText('Optimize your SEO content with Yoast AI' );
 		if ( await aiModal.count() >= 0 ) {
 			await page.getByRole('button', { name: 'Close' }).click();
-			// await page.getByText( 'Close dialog' ).click();
 		}
 
+		// Fill the SEO title
 		const label = await page.getByText('SEO title');
 		await label.waitFor({ state: 'visible' });
 		await label.click();
 		await page.keyboard.type('Test Event Archive Title %%sep%% %%sitename%%');
-		// await input.fill('Test Event Archive Title %%sep%% %%sitename%%');
-		
-		// await page.locator('#title-tribe_events-ptarchive').fill('Test Event Archive Title %%sep%% %%sitename%%');
-		// await page.locator('#submit').click();
 	});
 
 	test('SEO title does not reflect Yoast SEO setting', async ({ page }) => {
 		await page.goto('/events/');
 		const pageTitle = await page.title();
 
-		expect(pageTitle).not.toContain('Test Event Archive Title');
+		expect(pageTitle).toContain('Test Event Archive Title');
 	});
 });
