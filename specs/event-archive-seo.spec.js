0f5c195a2ee00de9e8aa8bfdef49553d4fd599a8
Dennis Claassen <declaassen@gmail.com>
2025-04-22 07:17:10 +0200

    Update event-archive-seo.spec.js

--- specs/event-archive-seo.spec.js
+++ specs/event-archive-seo.spec.js
@@ -8,14 +8,17 @@ test.describe('Yoast SEO on Events archive', () => {
 		// close ai modal
 		const aiModal = await page.getByText('Optimize your SEO content with Yoast AI' );
 		if ( await aiModal.count() > 0 ) {
+			await page.screenshot({ path: 'ai-modal.png', fullPage: true });
 			await page.getByRole('button', { name: 'Close' }).click();
 		}
 		
+		await page.screenshot({ path: 'settings-1st.png', fullPage: true });
 		console.log({ url: await page.url() });
 
 		//Navigate to events settings.
 		await page.locator('[id="link-post-type\\/event"]').click();
 
+		await page.screenshot({ path: 'event-settings.png', fullPage: true });
 		//await page.getByRole('link', { name: 'Events' }).click();
 
 		// Fill the SEO title
@@ -24,8 +27,12 @@ test.describe('Yoast SEO on Events archive', () => {
 		await label.click();
 		await page.keyboard.type('Test Event Archive Title %%sep%% %%sitename%%');
 		
+		await page.screenshot({ path: 'event-settings-changed.png', fullPage: true });
+		
 		// Save the changes.
 		await page.getByRole('button', { name: 'Save changes' }).click();
+		
+		await page.screenshot({ path: 'settings-saved.png', fullPage: true });
 	});
 	
 	test.afterEach(async ({ page }) => {
