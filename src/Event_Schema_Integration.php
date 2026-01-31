7df4c4cca6e4c558ff8feafc36491c739e7b2f0d
Dennis Claassen <declaassen@gmail.com>
2026-01-31 01:46:46 +0100

    Update Event_Schema_Integration.php

--- src/Event_Schema_Integration.php
+++ src/Event_Schema_Integration.php
@@ -6,4 +6,27 @@ class Event_Schema_Integration {
     public function register(): void {
         
     }
+
+    /**
+     * Enhance schema.org data describing an Event organizer.
+     *
+     * @template T
+     *
+     * @param T $event_data
+     * @param Meta_Tags_Context $context
+     *
+     * @return T|array{organizer:array{@id: string}}
+     */
+    function enhance_event_with_role( $event_data, $context ) {
+    	assert( $context instanceof Meta_Tags_Context );
+    
+    	if ( ! ( is_single() && get_post_type() === 'tribe_events' ) ) {
+    		return $event_data;
+    	}
+    
+    	$event_id = get_the_ID();
+    
+
+    	return $event_data;
+    }
 }
