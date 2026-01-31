d5dd9ef90d5b75007485869e84153fab54478943
Dennis Claassen <declaassen@gmail.com>
2026-02-01 00:50:29 +0100

    Update Event_Schema_Integration.php

--- src/Event_Schema_Integration.php
+++ src/Event_Schema_Integration.php
@@ -2,10 +2,9 @@
 
 namespace DC23\TheEventAttendee;
 
-use Yoast\WP\SEO\Config\Schema_IDs;
 use Yoast\WP\SEO\Context\Meta_Tags_Context;
 
-class Event_Schema_Integration {
+class Organizer_Schema_Integration {
     public function register(): void {
     	add_filter( 'wpseo_schema_event', [ $this, 'enhance_event_organizer' ], 11, 2 );
     }
