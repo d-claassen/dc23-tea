c47ec353812b197174f6ed96d08355502d99eda3
Dennis Claassen <declaassen@gmail.com>
2026-02-01 01:00:32 +0100

    Update dc23-tea.php

--- dc23-tea.php
+++ dc23-tea.php
@@ -44,61 +44,9 @@ function dc23_tea_block_init() {
 	register_block_type( __DIR__ . '/build/venue-url' );
 
 	( new \DC23\TheEventAttendee\Event_Schema_Integration() )->register();
+	( new \DC23\TheEventAttendee\Organizer_Schema_Integration() )->register();
 }
 add_action( 'init', 'dc23_tea_block_init' );
-
-function dc23_tea_schema_init() {
-	add_filter( 'wpseo_schema_event', 'enhance_event_with_role', 11, 2 );
-}
-add_action( 'init', 'dc23_tea_schema_init' );
-
-/**
- * Enhance schema.org data describing an Event with the role of the entity represented by the website.
- *
- * @template T
- *
- * @param T $event_data
- * @param Meta_Tags_Context $context
- *
- * @return T|array{attendee:array, organizer:array, sponsor:array, performer:array}
- */
-function enhance_event_with_role( $event_data, $context ) {
-	assert( $context instanceof Meta_Tags_Context );
-
-	if ( ! ( is_single() && get_post_type() === 'tribe_events' ) ) {
-		return $event_data;
-	}
-
-	$event_id = get_the_ID();
-
-	$roles = get_post_meta( $event_id, '_EventRole', true );
-	if ( empty( $roles ) ) {
-		return $event_data;
-	}
-
-	if ( $context->site_represents !== 'person' ) {
-		return $event_data;
-	}
-
-	$person_reference = [
-		'@id'  => YoastSEO()->helpers->schema->id->get_user_schema_id( $context->site_user_id, $context ),
-	];
-
-	if ( in_array( 'Attending', $roles, true ) && empty( $event_data['attendee'] ) ) {
-		$event_data['attendee'] = $person_reference;
-	}
-	if ( in_array( 'Organizing', $roles, true ) && empty( $event_data['organizer'] ) ) {
-		$event_data['organizer'] = $person_reference;
-	}
-	if ( in_array( 'Sponsoring', $roles, true ) && empty( $event_data['sponsor'] ) ) {
-		$event_data['sponsor'] = $person_reference;
-	}
-	if ( in_array( 'Performing', $roles, true ) && empty( $event_data['performer'] ) ) {
-		$event_data['performer'] = $person_reference;
-	}
-
-	return $event_data;
-}
 
 
 /**
